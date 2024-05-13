b07753067de4b83ec89fcc60c38509866b0d06fb
melobrunoti <melobrunoti@gmail.com>
2024-05-13 11:09:02 -0300

    reset css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryProvider } from '@/providers/query-client/ReactQueryProvider';
+import '@/styles/global.css';
 
 const queryClient = new QueryClient();
 
